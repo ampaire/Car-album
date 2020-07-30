6b5678a60057d13a294e8438906c8a0d3f8c3cd4
ampaire <femayapr@gmail.com>
2020-07-30 12:20:02 -0600

    Heroku setup

--- config/routes.rb
+++ config/routes.rb
@@ -1,8 +1,4 @@
 Rails.application.routes.draw do
-  namespace :api do
-    namespace :v1 do
-      resources :cars
-    end
-  end
+  resources :cars
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
 end
